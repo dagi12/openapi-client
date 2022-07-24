1e5415b85cd8903d958c9ae14de1a16f2cae1dd8
Eryk Mariankowski <dagi12@o2.pl>
2022-07-24 15:06:09 +0200

    gen fix

--- src/gen/js/genTypes.ts
+++ src/gen/js/genTypes.ts
@@ -1,83 +1,83 @@
-import { writeFileSync, join } from '../util';
-import { DOC, SP, ST, getDocType, getTSParamType } from './support';
+import {join, writeFileSync} from '../util'
+import {DOC, getDocType, getTSParamType, SP, ST} from './support'
 
 export default function genTypes(spec: ApiSpec, options: ClientOptions) {
-	const file = genTypesFile(spec, options);
-	writeFileSync(file.path, file.contents);
+	const file = genTypesFile(spec, options)
+	writeFileSync(file.path, file.contents)
 }
 
 export function genTypesFile(spec: ApiSpec, options: ClientOptions) {
-	const lines = [];
-	join(lines, renderHeader());
-	join(lines, renderDefinitions(spec, options));
+	const lines = []
+	join(lines, renderHeader())
+	join(lines, renderDefinitions(spec, options))
 	return {
 		path: `${options.outDir}/types.${options.language}`,
 		contents: lines.join('\n'),
-	};
+	}
 }
 
 function renderHeader() {
-	const lines = [];
-	lines.push(`// @ts-nocheck`);
-	lines.push(`/** @module types */`);
-	lines.push(`// Auto-generated, edits will be overwritten`);
-	lines.push(``);
-	return lines;
+	const lines = []
+	lines.push(`// @ts-nocheck`)
+	lines.push(`/** @module types */`)
+	lines.push(`// Auto-generated, edits will be overwritten`)
+	lines.push(``)
+	return lines
 }
 
 function renderDefinitions(spec: ApiSpec, options: ClientOptions): string[] {
-	const isTs = options.language === 'ts';
-	const defs = spec.definitions || {};
+	const isTs = options.language === 'ts'
+	const defs = spec.definitions || {}
 	const isolatedModules = (options.isolatedModules)
 	const typeLines = isTs && !isolatedModules ? [`namespace api {`] : []
-	const docLines = [];
+	const docLines = []
 	Object.keys(defs).forEach((name) => {
-		const def = defs[name];
+		const def = defs[name]
 		if (isTs) {
-			join(typeLines, renderTsType(name, def, options));
+			join(typeLines, renderTsType(name, def, options))
 		}
-		join(docLines, renderTypeDoc(name, def));
-	});
+		join(docLines, renderTypeDoc(name, def))
+	})
 	if (isTs) {
-		join(typeLines, renderTsDefaultTypes());
-		!isolatedModules && typeLines.push('}');
+		join(typeLines, renderTsDefaultTypes())
+		!isolatedModules && typeLines.push('}')
 	}
-	return isTs ? typeLines.concat(docLines) : docLines;
+	return isTs ? typeLines.concat(docLines) : docLines
 }
 
 function renderTsType(name, def, options) {
-	if (def.allOf) return renderTsInheritance(name, def.allOf, options);
+	if (def.allOf) return renderTsInheritance(name, def.allOf, options)
 	if (def.type !== 'object') {
-		console.warn(`Unable to render ${name} ${def.type}, skipping.`);
-		return [];
+		console.warn(`Unable to render ${name} ${def.type}, skipping.`)
+		return []
 	}
 
-	const lines = [];
+	const lines = []
 	if (def.description) {
-		lines.push(`/**`);
-		lines.push(DOC + def.description.trim().replace(/\n/g, `\n${DOC}${SP}`));
-		lines.push(` */`);
+		lines.push(`/**`)
+		lines.push(DOC + def.description.trim().replace(/\n/g, `\n${DOC}${SP}`))
+		lines.push(` */`)
 	}
-	lines.push(`export interface ${name} {`);
+	lines.push(`export interface ${name} {`)
 
-	const required = def.required || [];
-	const props = Object.keys(def.properties || {});
-	const requiredProps = props.filter((p) => !!~required.indexOf(p));
-	const optionalProps = props.filter((p) => !~required.indexOf(p));
+	const required = def.required || []
+	const props = Object.keys(def.properties || {})
+	const requiredProps = props.filter((p) => !!~required.indexOf(p))
+	const optionalProps = props.filter((p) => !~required.indexOf(p))
 
 	const requiredPropLines = requiredProps
 		.map((prop) => renderTsTypeProp(prop, def.properties[prop], true))
-		.reduce((a, b) => a.concat(b), []);
+		.reduce((a, b) => a.concat(b), [])
 
 	const optionalPropLines = optionalProps
 		.map((prop) => renderTsTypeProp(prop, def.properties[prop], false))
-		.reduce((a, b) => a.concat(b), []);
+		.reduce((a, b) => a.concat(b), [])
 
-	join(lines, requiredPropLines);
-	join(lines, optionalPropLines);
-	lines.push('}');
-	lines.push('');
-	return lines;
+	join(lines, requiredPropLines)
+	join(lines, optionalPropLines)
+	lines.push('}')
+	lines.push('')
+	return lines
 }
 
 function renderTsInheritance(
@@ -85,13 +85,13 @@ function renderTsInheritance(
 	allOf: any[],
 	options: ClientOptions
 ) {
-	verifyAllOf(name, allOf);
-	const ref = allOf[0];
-	const parentName = ref.$ref.split('/').pop();
-	const lines = renderTsType(name, allOf[1], options);
-	if (lines[0].startsWith('export interface')) lines.shift();
-	lines.unshift(`export interface ${name} extends ${parentName} {`);
-	return lines;
+	verifyAllOf(name, allOf)
+	const ref = allOf[0]
+	const parentName = ref.$ref.split('/').pop()
+	const lines = renderTsType(name, allOf[1], options)
+	if (lines[0].startsWith('export interface')) lines.shift()
+	lines.unshift(`export interface ${name} extends ${parentName} {`)
+	return lines
 }
 
 function renderTsTypeProp(
@@ -99,19 +99,19 @@ function renderTsTypeProp(
 	info: any,
 	required: boolean
 ): string[] {
-	const lines = [];
-	const type = getTSParamType(info, true);
+	const lines = []
+	const type = getTSParamType(info, true)
 	if (info.description) {
-		lines.push(`${SP}/**`);
+		lines.push(`${SP}/**`)
 		lines.push(
 			`${SP}${DOC}` +
-				(info.description || '').trim().replace(/\n/g, `\n${SP}${DOC}${SP}`)
-		);
-		lines.push(`${SP} */`);
+			(info.description || '').trim().replace(/\n/g, `\n${SP}${DOC}${SP}`)
+		)
+		lines.push(`${SP} */`)
 	}
-	const req = required ? '' : '?';
-	lines.push(`${SP}${prop}${req}: ${type}${ST}`);
-	return lines;
+	const req = required ? '' : '?'
+	lines.push(`${SP}${prop}${req}: ${type}${ST}`)
+	return lines
 }
 
 function renderTsDefaultTypes() {
@@ -207,7 +207,7 @@ export interface ServiceOptions {
    *
    * You can use this to augment each request, for example add extra query parameters.
    *
-   *     const params = reqInfo.parameters;
+   *     const params = reqInfo.parameters
    *     if (params && params.query) {
    *       params.query.lang = "en"
    *     }
@@ -221,7 +221,7 @@ export interface ServiceOptions {
    * The response is promise based so simply resolve the "res" parameter
    * if you're happy with it e.g.
    *
-   *     if (!res.error) return Promise.resolve({ res });
+   *     if (!res.error) return Promise.resolve({ res })
    *
    * Otherwise return a promise which flags a retry.
    *
@@ -325,44 +325,44 @@ export interface ServiceMeta {
 export type AsyncAction = (dispatch: (action: any) => void) => Promise<void>${ST}
 `
 		.replace(/  /g, SP)
-		.split('\n');
+		.split('\n')
 }
 
 function renderTypeDoc(name: string, def: any): string[] {
-	if (def.allOf) return renderDocInheritance(name, def.allOf);
+	if (def.allOf) return renderDocInheritance(name, def.allOf)
 	if (def.type !== 'object') {
-		console.warn(`Unable to render ${name} ${def.type}, skipping.`);
-		return [];
+		console.warn(`Unable to render ${name} ${def.type}, skipping.`)
+		return []
 	}
 
-	const group = 'types';
+	const group = 'types'
 	const lines = [
 		'/**',
 		`${DOC}@typedef ${name}`,
 		`${DOC}@memberof module:${group}`,
-	];
-	const req = def.required || [];
-	const propLines = Object.keys(def.properties).map((prop) => {
-		const info = def.properties[prop];
+	]
+	const req = def.required || []
+	const propLines = !!def.properties ? Object.keys(def.properties).map((prop) => {
+		const info = def.properties[prop]
 		const description = (info.description || '')
 			.trim()
-			.replace(/\n/g, `\n${DOC}${SP}`);
-		return `${DOC}@property {${getDocType(info)}} ${prop} ${description}`;
-	});
-	if (propLines.length) lines.push(`${DOC}`);
-	join(lines, propLines);
-	lines.push(' */');
-	lines.push('');
-	return lines;
+			.replace(/\n/g, `\n${DOC}${SP}`)
+		return `${DOC}@property {${getDocType(info)}} ${prop} ${description}`
+	}) : [];
+	if (propLines.length) lines.push(`${DOC}`)
+	join(lines, propLines)
+	lines.push(' */')
+	lines.push('')
+	return lines
 }
 
 function renderDocInheritance(name: string, allOf: any[]) {
-	verifyAllOf(name, allOf);
-	const ref = allOf[0];
-	const parentName = ref.$ref.split('/').pop();
-	const lines = renderTypeDoc(name, allOf[1]);
-	lines.splice(3, 0, `${DOC}@extends ${parentName}`);
-	return lines;
+	verifyAllOf(name, allOf)
+	const ref = allOf[0]
+	const parentName = ref.$ref.split('/').pop()
+	const lines = renderTypeDoc(name, allOf[1])
+	lines.splice(3, 0, `${DOC}@extends ${parentName}`)
+	return lines
 }
 
 function verifyAllOf(name: string, allOf: any[]) {
@@ -370,15 +370,15 @@ function verifyAllOf(name: string, allOf: any[]) {
 	// but seems to be how most model inheritance in Swagger and is consistent
 	// with other code generation tool
 	if (!allOf || allOf.length !== 2) {
-		console.log(allOf);
+		console.log(allOf)
 		throw new Error(
 			`Json schema allOf '${name}' must have two elements to be treated as inheritance`
-		);
+		)
 	}
-	const ref = allOf[0];
+	const ref = allOf[0]
 	if (!ref.$ref) {
 		throw new Error(
 			`Json schema allOf '${name}' first element must be a $ref ${ref}`
-		);
+		)
 	}
 }
